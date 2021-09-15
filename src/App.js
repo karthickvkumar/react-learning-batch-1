407f5fc2807c85a2d0222e6494fb66b691b30b74
kvelraj_concord <kvelraj@concord.net>
2021-09-15 10:35:19 +0530

    react

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, {Component} from 'react';
 
 class App extends Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      message : "This is a state message"
+    }
+  }
+
+  display(){
+    // console.log("Hello, Welcome to React")
+    this.setState({
+      message : "Hello, Welcome to React"
+    })
+  }
+
   render(){
     return(
       <div>
         <h1>Hello! Welcome to React Learning</h1>
         <h1>Tutorial by Karthick</h1>
+        <h1>{this.state.message}</h1>
+        <button onClick={() => this.display()}>Run the function</button>
       </div>
     )
   }
