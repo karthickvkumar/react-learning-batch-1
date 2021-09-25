606c735f0180bd033b1df049d935b2456ba96252
kvelraj_concord <kvelraj@concord.net>
2021-09-25 11:28:15 +0530

    get api

--- src/pages/inbox-page.js
+++ src/pages/inbox-page.js
@@ -3,13 +3,25 @@ import axios from "axios";
 
 class Inbox extends Component{
 
+  constructor(props){
+    super(props);
+    this.state = {
+      userList : []
+    }
+  }
+
   listUser(){
     const url = "https://reqres.in/api/users?page=2";
 
     axios.get(url)
       .then((response) => {
         // Success Code Block
-        console.log(response.data)
+        const serverData = response.data;
+        console.log(serverData.data)
+        this.setState({
+          userList : serverData.data
+        })
+
       })
       .catch((error) => {
         // Error Code Block
@@ -18,10 +30,41 @@ class Inbox extends Component{
   }
 
   render(){
+
+    const user = this.state.userList.map((value, index) => {
+      return(
+        <tr key={index}>
+          <td>{value.id}</td>
+          <td>{value.first_name}</td>
+          <td>{value.last_name}</td>
+          <td>{value.email}</td>
+          <td>
+            <img src={value.avatar} alt={value.avatar} className="profile-image"/>
+          </td>
+        </tr>
+      )
+    })
+
     return(
       <div>
         <h1>This is a Inbox Page</h1>
         <button onClick={() => this.listUser()}>List All Users</button>
+        <table id="customers">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Profile Picture</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {user}
+   
+          </tbody>
+        </table>
       </div>
     )
   }
--- src/pages/login-page.js
+++ src/pages/login-page.js
@@ -26,9 +26,9 @@ class LoginPage extends Component{
       <div>
         <h1 className="heading">Welcome to Login Page</h1>
 
-        <img src={require("../images/shinchu.jpg").default} style={{'width': '250px'}}/>
+        <img src={require("../images/shinchu.jpg").default} style={{'width': '250px'}} alt="local"/>
 
-        <img className="car-img" src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d35eacaf1176b0008974b54%2F2020-Chevrolet-Corvette-Stingray%2F960x0.jpg%3FcropX1%3D790%26cropX2%3D5350%26cropY1%3D784%26cropY2%3D3349"/>
+        <img className="car-img" src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d35eacaf1176b0008974b54%2F2020-Chevrolet-Corvette-Stingray%2F960x0.jpg%3FcropX1%3D790%26cropX2%3D5350%26cropY1%3D784%26cropY2%3D3349" alt="url"/>
         
         <div className="bottom-space">
           <label className="lable-Text">Enter your Email Id :</label>
